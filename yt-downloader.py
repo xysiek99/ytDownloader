b00bc6e850da880c88a35573eaac3756ddcc2bf9
Krzysiek <xysiek99@gmail.com>
2021-04-09 01:23:24 +0200

    Created option choosing to download sound/video

--- yt-downloader.py
+++ yt-downloader.py
@@ -1,10 +1,26 @@
 import youtube_dl as yt
 
-yt_ops = {'outtmpl': '%(id)s.%(ext)s'}
-video = yt.YoutubeDL(yt_ops)
-url = "https://www.youtube.com/watch?v=7Kofui217Vk&ab_channel=MatthewOlson"
+video_options = {'outtmpl': '%(id)s.%(ext)s'}
+sound_options = {'format': 'bestaudio'}
+
+video = yt.YoutubeDL(video_options)
+sound = yt.YoutubeDL(sound_options)
+
+url = "https://www.youtube.com/watch?v=ua7TBlFbBjg&ab_channel=Franck"
 
 vinfo = video.extract_info(url, download=False)
-print(vinfo)
 
-#video.download(["https://www.youtube.com/watch?v=7Kofui217Vk&ab_channel=MatthewOlson"])
+# How to get information about the video
+# As it is a dict use below instead of: vinfo.title
+'''
+print(vinfo)
+print(vinfo['title']) 
+'''
+
+video.download([url])
+sound.download([url])
+
+# USEFUL LINKS:
+#
+# https://stackoverflow.com/questions/49246598/youtube-dl-get-audio-link-with-python
+# https://github.com/ytdl-org/youtube-dl/blob/master/README.md#embedding-youtube-dl
