13fdf75c982d83f776e3dab82c349bd8f5d27861
Krzysiek <xysiek99@gmail.com>
2021-04-15 22:22:41 +0200

    Added function to return youtube link's title

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,23 +1,5 @@
 import youtube_dl as yt
 
-video_options = {'outtmpl': '%(title)s.%(ext)s'}
-sound_options = {'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'}
-
-#video = yt.YoutubeDL(video_options)
-#sound = yt.YoutubeDL(sound_options)
-
-url = "https://www.youtube.com/watch?v=BOYdZ0GyQFQ&ab_channel=PearlJam"
-chuj = "https://www.youtube.com/watch?v=BOYdZ0GyQFQ&ab_channel=PearlJam"
-
-# How to get information about the video
-# As it is a dict use below instead of: vinfo.title
-'''
-vinfo = video.extract_info(url, download=False)
-print(vinfo['title'])
-print(vinfo['ext'])
-print(vinfo['format']) 
-'''
-
 # USEFUL LINKS:
 #
 # https://stackoverflow.com/questions/49246598/youtube-dl-get-audio-link-with-python
