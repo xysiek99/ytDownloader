7617a5ec00d2e22d248a7178156b83947be85371
Krzysiek <xysiek99@gmail.com>
2021-04-21 00:30:09 +0200

    Created function to check if url exists and is YT's url

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,16 +1,11 @@
 import youtube_dl as yt
+import urllib
 
 # USEFUL LINKS:
 #
 # https://stackoverflow.com/questions/49246598/youtube-dl-get-audio-link-with-python
 # https://github.com/ytdl-org/youtube-dl/blob/master/README.md#embedding-youtube-dl
 # https://github.com/ytdl-org/youtube-dl/blob/master/README.md#configuration
-
-def ignoreErrors():
-    # AttributeError            IGNORE
-    # socket.gaierror           show message
-    # urllib.error.URLError     show message
-    pass
 
 def downloadItem(link, type):    
     if type == "video":
@@ -24,3 +19,11 @@ def downloadItem(link, type):
 def getTitle(link):
     material = yt.YoutubeDL().extract_info(url=link, download=False)
     return material['title']
+
+def checkUrl(link):
+    try:
+        response = urllib.request.urlopen(link)
+        if link.startswith("https://www.youtube") and response.code == 200:
+            return True
+    except:
+        return False
